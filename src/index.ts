3cc2594d924b2e0d0299bb75a67e798c27f6f8ab
plongtran239 <plongtran239@gmail.com>
2023-06-05 17:13:05 +0700

    Connect to Mongo Atlas by MongoDB Driver

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import express from 'express'
 // Route handlers
 import usersRouter from './routes/users.routes'
 
+// Services
+import databaseService from './services/database.services'
+
 const PORT = 4000
 
 const app = express()
@@ -11,6 +14,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/users', usersRouter)
+
+databaseService.connect().catch(console.dir)
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
